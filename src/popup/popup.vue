fd151f0570e051e7e16c0fedc6ecccea644a5f80
Yoshihiro Tanaka <contact@cordea.jp>
2021-01-23 17:58:42 +0900

    add new component

---
+++ src/popup/popup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+const PopupProps = Vue.extend({
+  props: {
+    url: String
+  }
+});
+
+@Component
+export default class Popup extends PopupProps {
+  mounted() {
+  }
+}
+</script>
